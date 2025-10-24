c9a47000e84e552eb26fcf8f3d55099082adb2ff
cmgjn1881 <cmgjn1881@naver.com>
2025-10-25 00:39:21 +0900

    fix: webSecurityCustomizer의 무시 목록 수정

--- src/main/java/com/lineupmaker/config/SecurityConfig.java
+++ src/main/java/com/lineupmaker/config/SecurityConfig.java
@@ -52,7 +52,8 @@ public class SecurityConfig {
                 "/api/auth/signup",
                 "/api/auth/login",
                 "/api/auth/refresh",
-                "/api/auth/logout"
+                "/api/auth/logout",
+                "/api/auth/token/exchange"
         );
     }
 
